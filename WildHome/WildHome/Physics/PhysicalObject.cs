8a37ee86f8e0ba91fe8a6ef709d09ceece7c79fe
Unknown <cascales.arthur@gmail.com>
2016-11-30 22:43:12 +0100

    Ajout variable PhysicalObject
    Commencement de la création du physicalObject

--- WildHome/WildHome/Physics/PhysicalObject.cs
+++ WildHome/WildHome/Physics/PhysicalObject.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace WildHome.Physics
 {
     class PhysicalObject
     {
+        //VARIABLE
+        protected float _mass;
+        protected float _angle;
+
+        protected Vector2 _position;
+        protected Vector2 _speed;
+        protected Vector2 _acceleration;
+
+        private World _world;
+
+        //CONSTRUCTOR
+        public PhysicalObject(float mass)
+        {
+            this._mass = mass;
+            this._angle = 0;
+
+            this._position = Vector2.Zero;
+            this._speed = Vector2.Zero;
+            this._acceleration = Vector2.Zero;
+        }
+
     }
 }
