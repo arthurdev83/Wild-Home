1f8e7d1380889906508c69587a134465944c81f2
Unknown <cascales.arthur@gmail.com>
2016-12-01 08:48:58 +0100

    Modification world
    Ajout des variables de bases + addPhysicalObject()

--- WildHome/WildHome/Physics/World.cs
+++ WildHome/WildHome/Physics/World.cs
@@ -8,5 +8,29 @@ namespace WildHome.Physics
 {
     class World
     {
+        //VARIABLE
+        private float _gravity;
+
+        private List<PhysicalObject> physicalObjects;
+
+        //PROPERTY
+        public float Gravity
+        {
+            get { return this._gravity; }
+            set { this._gravity = value; }
+        }
+
+        //CONSTRUCTOR
+        public World(float gravity = 9.8f)
+        {
+            this.physicalObjects = new List<PhysicalObject>();
+            this._gravity = gravity;
+        }
+
+        //Add Physical Object to World
+        public void AddPhysicalObject(PhysicalObject physicalObject)
+        {
+            this.physicalObjects.Add(physicalObject);
+        }
     }
 }
